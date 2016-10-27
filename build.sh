59ab0f8d3f0878cb9a065929c819cf149ab929ac
David Kerkeslager <kerkeslager@gmail.com>
2016-10-27 22:19:58 +0000

    Very basic multiprocess program

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -pthread -o cayenne main.c
---
+++ main.c
@@ -0,0 +1,54 @@
+/* This file is part of Cayenne.
+
+Cayenne is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cayenne is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cayenne.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define PTHREAD_COUNT 10
+
+void* driverLoop(void* arg)
+{
+  int threadId;
+  threadId = *((int*) arg);
+  printf("Greetings from thread %d!\n", threadId);
+  return NULL;
+}
+
+void startPThreads()
+{
+  pthread_t threads[PTHREAD_COUNT];
+  int thread_args[PTHREAD_COUNT];
+  int rc, i;
+
+  /* spawn the threads */
+  for(i = 0; i < PTHREAD_COUNT; i++)
+  {
+    thread_args[i] = i;
+    rc = pthread_create(&threads[i], NULL, driverLoop, (void*) &thread_args[i]);
+  }
+
+  /* wait for threads to finish */
+  for (i = 0; i < PTHREAD_COUNT; i++)
+  {
+    rc = pthread_join(threads[i], NULL);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  startPThreads();
+  return 0;
+}
